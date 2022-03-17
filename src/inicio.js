1362b5190a4c7820b9d46bf44c5631ae463d5607
Alejandra <alejandra7.moo@gmail.com>
2022-03-16 19:58:22 -0600

    Primera App

---
+++ src/inicio.js
@@ -0,0 +1,15 @@
+function inicio () {
+    //JSK: mazcla de HTML y JavaScript
+    return(
+        <div>
+      <h1>¡Bienvenido a React!</h1>
+      <p>Esta a punto de iniciar un emocionante viaje en el que creara aplicaciones web usando las tecnologias mas recientes.</p>
+        
+  
+    </div>
+  );
+}
+
+}
+
+export default inicio;
