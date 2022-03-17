06c153e4e2c7a4665f3316fbb9635df2fe4ba842
Alejandra <alejandra7.moo@gmail.com>
2022-03-16 20:04:05 -0600

    Primera app

--- src/inicio.js
+++ src/inicio.js
@@ -1,15 +1,17 @@
+import App from "./App";
+
 function inicio () {
     //JSK: mazcla de HTML y JavaScript
     return(
-        <div>
-      <h1>¡Bienvenido a React!</h1>
-      <p>Esta a punto de iniciar un emocionante viaje en el que creara aplicaciones web usando las tecnologias mas recientes.</p>
+     <div>
+       <h1>¡Bienvenido a React!</h1>
+       <p>Esta a punto de iniciar un emocionante viaje en el que creara aplicaciones web usando las tecnologias mas recientes.</p>
         
   
-    </div>
+     </div>
   );
 }
 
 }
 
-export default inicio;
+export default App;
